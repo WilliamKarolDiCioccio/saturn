0a35e9cfe3ae476b6be7f7d5a2d3083cbfc40aa2
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2026-02-04 19:30:55 +0100

    fix: removed uninitialized deprecated members

--- codex/include/codex/source_extractor.hpp
+++ codex/include/codex/source_extractor.hpp
@@ -12,10 +12,6 @@ namespace codex
 
 class SourceExtractor
 {
-   private:
-    std::filesystem::path m_path;
-    std::string m_sourceCode;
-
    public:
     std::shared_ptr<Source> extractSource(const std::filesystem::path& _path);
 };
--- codex/src/source_extractor.cpp
+++ codex/src/source_extractor.cpp
@@ -11,19 +11,19 @@ namespace codex
 
 std::shared_ptr<Source> SourceExtractor::extractSource(const std::filesystem::path& _path)
 {
-    std::ifstream file(m_path);
+    std::ifstream file(_path);
     if (!file.is_open())
     {
-        std::cerr << "Failed to open file: " << m_path << "\n";
+        std::cerr << "Failed to open file: " << _path << "\n";
     }
 
     std::stringstream buffer;
     buffer << file.rdbuf();
-    m_sourceCode = buffer.str();
+    std::string sourceCode = buffer.str();
 
     auto source = std::make_shared<Source>(
-        m_path.filename().string(), m_path, m_sourceCode, "UTF-8",
-        std::filesystem::last_write_time(m_path).time_since_epoch().count());
+        _path.filename().string(), _path, sourceCode, "UTF-8",
+        std::filesystem::last_write_time(_path).time_since_epoch().count());
 
     return source;
 }
